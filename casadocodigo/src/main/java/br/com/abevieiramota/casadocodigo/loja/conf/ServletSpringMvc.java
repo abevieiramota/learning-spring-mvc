f87f87d4113dfb03a417a04b1d17e62001690bb0
Abelardo Vieira Mota <abevieiramota@gmail.com>
2016-11-14 19:19:55 -0200

    Adiciona comentários

--- casadocodigo/src/main/java/br/com/abevieiramota/casadocodigo/loja/conf/ServletSpringMvc.java
+++ casadocodigo/src/main/java/br/com/abevieiramota/casadocodigo/loja/conf/ServletSpringMvc.java
@@ -2,19 +2,23 @@ package br.com.abevieiramota.casadocodigo.loja.conf;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+// Web application initializer
 public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
+	// classes de configuração para root
 	protected Class<?>[] getRootConfigClasses() {
 		return null;
 	}
 
 	@Override
+	// classes de configuração para o dispatcher
 	protected Class<?>[] getServletConfigClasses() {
 		return new Class[] {AppWebConfiguration.class};
 	}
 
 	@Override
+	// paths para o dispatcher
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
 	}
--- casadocodigo/src/main/java/br/com/abevieiramota/casadocodigo/loja/controllers/HomeController.java
+++ casadocodigo/src/main/java/br/com/abevieiramota/casadocodigo/loja/controllers/HomeController.java
@@ -3,13 +3,16 @@ package br.com.abevieiramota.casadocodigo.loja.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// define um controlador
 @Controller
 public class HomeController {
 	
+	// mapeia uma request num handler
 	@RequestMapping("/")
 	public String index() {
 		System.out.println("Entrou na home.");
 		
+		// nome de view
 		return "home";
 	}
 }
